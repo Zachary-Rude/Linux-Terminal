ec708752d4e69bafc23307878fbf688830db24f1
Zachary-Rude <84681078+Zachary-Rude@users.noreply.github.com>
2021-06-10 16:40:33 -0700

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ import os
 pink = "\033[0;31m"
 white = "\033[0;37m"
 while True:
-    print(pink + "chromeready@penguin:~$", end=" ")
-    cmd = input(white)
-    os.system(cmd)
+     print(pink + "zacharyrude@ubuntu:~$", end=" ")
+     cmd = input(white)
+     os.system(cmd)
